02b655cfde6e3aaef09ccdcc9b9b1650c3afb4db
Blue Draagon <khalifaasene10@gmail.com>
2025-03-02 15:11:09 +0000

    advance use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,25 @@
 import {useState} from "react";
 
 function App() {
+    // hook must not be on if or loop
     const [count, setCount] = useState(0)
+    const [person, setPerson] = useState({
+        firstname: "John",
+        lastname: "Doe",
+        age: 25,
+    })
     const increment = () => {
-        setCount(count + 1)
+        setPerson({...person, age: person.age + 1});
+    }
+    const incrementCount = () => {
+        setCount(count + 1);
     }
     return (
         <>
             <p>Counter : {count}</p>
-            <button onClick={increment}>Increment</button>
+            <p>Age : {person.age}</p>
+            <button onClick={incrementCount}>Increment</button>
+            <button onClick={increment}>Gagner une année</button>
         </>
     )
 }
