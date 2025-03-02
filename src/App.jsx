daf3225a81c162d5a3bed8f9ffc4b3db77e1aa13
Blue Draagon <khalifaasene10@gmail.com>
2025-03-02 15:02:23 +0000

    simple use state

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
+import {useState} from "react";
+
 function App() {
-    const handleClick = (e) => {
-        alert('clicked')
+    const [count, setCount] = useState(0)
+    const increment = () => {
+        setCount(count + 1)
     }
-    const showTitle = true;
-    const todos = [
-        "apprendre react",
-        "code l'application multiservice",
-        "code l'application de gestion",
-    ]
     return (
-    <>
-        { showTitle && <h1>Bonjour les gens</h1> }
-        <input type="text"/>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab amet consequuntur, cumque deserunt dicta distinctio dolorem doloribus ducimus explicabo fugiat in magnam neque quasi qui sunt voluptate voluptatibus. Quidem, sit?
-        </p>
-        <ul>
-            { todos.map(todo => (
-                <li key={todo}>{todo}</li>
-            ))}
-        </ul>
-    </>
-  )
+        <>
+            <p>Counter : {count}</p>
+            <button onClick={increment}>Increment</button>
+        </>
+    )
 }
 
 export default App
