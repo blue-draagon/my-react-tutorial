903078c35a04287be2cf97f650b324eab6e07c76
Blue Draagon <khalifaasene10@gmail.com>
2025-03-02 15:22:56 +0000

    controled input

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,18 @@ function App() {
         setName(e.target.value);
     }
 
+    const reset = () => {
+        setName("")
+    }
+
+    // controlled input is better for input
+    // that we want reaction for every change
     return (
         <>
             <form>
-                <input type="text" name="name" value={name} onChange={handleChange} />
+                <input type="text" name="name" value={name} onChange={handleChange}/>
                 {name}
+                <button type="button" onClick={reset}>Reset</button>
             </form>
         </>
     )
