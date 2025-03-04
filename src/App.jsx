f3f2af42e570c2b7908915ef0f568313299e88bd
Blue Draagon <khalifaasene10@gmail.com>
2025-03-04 15:32:51 +0000

    using ref to catch a div

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import {use, useMemo, useRef, useState} from "react";
+import {use, useEffect, useMemo, useRef, useState} from "react";
 import {Input} from "./components/forms/Input.jsx";
 
 function App() {
 
     const ref = useRef(null);
 
-    console.log(ref);
+    useEffect(() => {
+        console.log(ref.current.offsetHeight);
+    }, []);
 
     return (
-        <div onClick={() => ref.current = 'Clicked'}>
+        <div ref={ref}>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet corporis dolores exercitationem obcaecati quia saepe. Ad, animi commodi ducimus ea iure mollitia natus officia omnis voluptas voluptatibus. Cupiditate, dolorum, laborum!
         </div>
     )
