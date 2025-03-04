8e86e6ca533b2bde1d4957b056fe3057566d45f4
Blue Draagon <khalifaasene10@gmail.com>
2025-03-04 16:00:19 +0000

    advence usage of ref

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import {Input} from "./components/forms/Input.jsx";
 function App() {
 
     const ref = useRef(null);
+    const [value, setValue] = useState("");
+    ref.current = value
 
     useEffect(() => {
-        console.log(ref.current.offsetHeight);
+        const timer = setInterval(() => {
+            console.log(ref.current)
+        }, 1000)
+
+        return () => {
+            clearInterval(timer);
+        }
     }, []);
 
     return (
-        <div ref={ref}>
+        <div className="container my-3">
+            <Input
+                label="Prefix"
+                value={value}
+                onChange={setValue}
+            />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet corporis dolores exercitationem obcaecati quia saepe. Ad, animi commodi ducimus ea iure mollitia natus officia omnis voluptas voluptatibus. Cupiditate, dolorum, laborum!
         </div>
     )
